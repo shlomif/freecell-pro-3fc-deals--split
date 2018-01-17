dbd3246389174eb89cd9fe7816122be721fd027d
Shlomi Fish <shlomif@shlomifish.org>
2018-01-17 20:19:47 +0200

    Add a tidyall test.

--- bin/old/split.pl
+++ bin/old/split.pl
@@ -2,20 +2,24 @@
 
 use strict;
 use warnings;
+use autodie;
 
 use Path::Tiny qw/ path /;
 
 open my $in, '<', 'freecell-pro-3fc-deals/Imp3.txt';
 my $old_fn;
 my $fh;
-while (my $l = <$in>)
+while ( my $l = <$in> )
 {
-    my $z = sprintf("%010d", int $l);
-    my $fn = "freecell-pro-3fc-deals--split/Imp3/" . substr($z, 0, 3) . "XXXXXXX.txt";
-    if ($fn ne $old_fn)
+    my $z = sprintf( "%010d", int $l );
+    my $fn =
+          "freecell-pro-3fc-deals--split/Imp3/"
+        . substr( $z, 0, 3 )
+        . "XXXXXXX.txt";
+    if ( $fn ne $old_fn )
     {
         $old_fn = $fn;
-        $fh = path($fn);
+        $fh     = path($fn);
     }
     $fh->append($l);
 }
